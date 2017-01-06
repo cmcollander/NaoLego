1882360ff16f18aeed78855caa3b2d6744a2505e
Chris Collander <cmcollander@gmail.com>
2017-01-06 09:47:46 -0600

    Adjusting Threshold

--- NaoLego.py
+++ NaoLego.py
@@ -437,7 +437,7 @@ def verifyBlocks():
 # This function handles the actual threshold comparison
 def compareThreshold(norm):
 	n = int(norm)
-	thresh = 22000 + 600*getOpenConnectorCount()
+	thresh = 20000 + 600*getOpenConnectorCount()
 	comp = '='
 	if n<thresh:
 		comp = '<'
