c6740e889c00ba35b6c036475d14ad2b89128dfd
Chris Collander <cmcollander@gmail.com>
2017-01-10 19:41:25 -0600

    New CrossValidation Parameters

--- crossValidation.py
+++ crossValidation.py
@@ -16,8 +16,12 @@ with open('data.csv','rb') as csvfile:
 				x.append(row[:-1])
 				y.append(row[-1])
 
-parameters = {'min_samples_leaf':range(1,21),'criterion':['gini','entropy'],'max_depth':range(3,20)}
-clf = grid_search.GridSearchCV(tree.DecisionTreeClassifier(),parameters,n_jobs=4)
-clf.fit(X=x,y=y)
+parameters = {'splitter':['best','random'],'min_samples_leaf':range(1,21),'criterion':['gini','entropy'],'max_depth':range(3,20)}
+# specify parameters and distributions to sample from
+param_dist = {
+	"min_samples_leaf": range(1, 51),
+	"criterion": ["gini", "entropy"]}
+clf = grid_search.GridSearchCV(tree.DecisionTreeClassifier(),param_dist,n_jobs = 8)
+clf.fit(x,y)
 tree_model = clf.best_estimator_
 print (clf.best_score_,clf.best_params_)
