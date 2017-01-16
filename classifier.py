ba77132dea1c0b4864763a9fb53d558c6464eeff
Chris Collander <cmcollander@gmail.com>
2017-01-16 11:54:44 -0600

    New Data

--- classifier.py
+++ classifier.py
@@ -1,23 +1,5 @@
 def tree(Conns, OpenConnectors, Layers, yValue, diff):
 	if diff <= 23535.5:
-		if yValue <= 168.5:
-			return False
-		else:  # if yValue > 168.5
-			if diff <= 21993.0:
-				return True
-			else:  # if diff > 21993.0
-				if diff <= 22874.0:
-					return False
-				else:  # if diff > 22874.0
-					return True
+		return True
 	else:  # if diff > 23535.5
-		if yValue <= 335.0:
-			return False
-		else:  # if yValue > 335.0
-			if OpenConnectors <= 5.5:
-				return False
-			else:  # if OpenConnectors > 5.5
-				if diff <= 31453.0:
-					return True
-				else:  # if diff > 31453.0
-					return False
+		return False
